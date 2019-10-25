34e7f2ce698ffd282c57d638d994577a7e93e20e
Michail Bellas <michail.bellas@eurodyn.com>
2019-10-25 13:25:21 +0300

    Expose endpoints

--- qlack-webdesktop-app/src/main/java/com/eurodyn/qlack/webdesktop/WebDesktopApplication.java
+++ qlack-webdesktop-app/src/main/java/com/eurodyn/qlack/webdesktop/WebDesktopApplication.java
@@ -6,12 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 @SpringBootApplication
 @EnableJpaRepositories({"com.eurodyn.qlack.webdesktop.repository"})
 @EntityScan({"com.eurodyn.qlack.webdesktop.model"})
 @ComponentScan(basePackages = {
         "com.eurodyn.qlack.webdesktop.configuration",
+        "com.eurodyn.qlack.webdesktop.controller",
         "com.eurodyn.qlack.webdesktop.mapper",
         "com.eurodyn.qlack.webdesktop.service",
         "com.eurodyn.qlack.fuse.crypto.service"
