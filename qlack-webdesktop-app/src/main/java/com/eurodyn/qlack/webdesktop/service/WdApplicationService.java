03cddcbc4f351b338f899f1989fcb45177c0be23
Margarita Tryfou <mtrifou@gmail.com>
2019-11-08 18:08:28 +0200

    Small fix in menu filtering

--- qlack-webdesktop-app/src/main/java/com/eurodyn/qlack/webdesktop/service/WdApplicationService.java
+++ qlack-webdesktop-app/src/main/java/com/eurodyn/qlack/webdesktop/service/WdApplicationService.java
@@ -1,14 +1,14 @@
 package com.eurodyn.qlack.webdesktop.service;
 
-        import com.eurodyn.qlack.webdesktop.dto.WdApplicationDTO;
-        import com.eurodyn.qlack.webdesktop.mapper.WdApplicationMapper;
-        import com.eurodyn.qlack.webdesktop.model.WdApplication;
-        import com.eurodyn.qlack.webdesktop.repository.WdApplicationRepository;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.stereotype.Service;
-        import org.springframework.validation.annotation.Validated;
+import com.eurodyn.qlack.webdesktop.dto.WdApplicationDTO;
+import com.eurodyn.qlack.webdesktop.mapper.WdApplicationMapper;
+import com.eurodyn.qlack.webdesktop.model.WdApplication;
+import com.eurodyn.qlack.webdesktop.repository.WdApplicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
-        import java.util.List;
+import java.util.List;
 
 /**
  * Provides Qlack Web Desktop application related functionality
@@ -19,41 +19,39 @@ package com.eurodyn.qlack.webdesktop.service;
 @Validated
 public class WdApplicationService {
 
-  private WdApplicationMapper mapper;
-  private WdApplicationRepository wdApplicationRepository;
+    private WdApplicationMapper mapper;
+    private WdApplicationRepository wdApplicationRepository;
 
-  @Autowired
-  public WdApplicationService(WdApplicationMapper mapper,
-                              WdApplicationRepository wdApplicationRepository) {
-    this.mapper = mapper;
-    this.wdApplicationRepository = wdApplicationRepository;
-  }
+    @Autowired
+    public WdApplicationService(WdApplicationMapper mapper,
+                                WdApplicationRepository wdApplicationRepository) {
+        this.mapper = mapper;
+        this.wdApplicationRepository = wdApplicationRepository;
+    }
 
-  /**
-   * Finds all active Web Desktop applications
-   *
-   * @return a list of {@link com.eurodyn.qlack.webdesktop.dto.WdApplicationDTO}
-   */
-  public List<WdApplicationDTO> findAllActiveApplications() {
-    return mapper.mapToDTO(wdApplicationRepository.findByActiveIsTrue());
-  }
+    /**
+     * Finds all active Web Desktop applications
+     *
+     * @return a list of {@link com.eurodyn.qlack.webdesktop.dto.WdApplicationDTO}
+     */
+    public List<WdApplicationDTO> findAllActiveApplications() {
+        return mapper.mapToDTO(wdApplicationRepository.findByActiveIsTrue());
+    }
 
-  /**
-   * Finds all active Web Desktop applications with trimmed group names.
-   *
-   * @return a list of {@link com.eurodyn.qlack.webdesktop.dto.WdApplicationDTO}
-   */
-  public List<WdApplicationDTO> findAllActiveApplicationsFilterGroupName() {
-    List<WdApplication> wdApplicationList = wdApplicationRepository.findByActiveIsTrue();
-    wdApplicationList.forEach(wdApplication -> {
-      if (wdApplication.getGroupName() != null) {
+    /**
+     * Finds all active Web Desktop applications with trimmed group names.
+     *
+     * @return a list of {@link com.eurodyn.qlack.webdesktop.dto.WdApplicationDTO}
+     */
+    public List<WdApplicationDTO> findAllActiveApplicationsFilterGroupName() {
+        List<WdApplication> wdApplicationList = wdApplicationRepository.findByActiveIsTrue();
+        wdApplicationList.forEach(wdApplication -> {
+            if (wdApplication.getGroupName() == null || "null".equals(wdApplication.getGroupName())) {
+                wdApplication.setGroupName("");
+            }
             wdApplication.setGroupName(wdApplication.getGroupName().trim());
-        }
-        if ("null".equals(wdApplication.getGroupName())) {
-            wdApplication.setGroupName("");
-        }
-    });
-    return mapper.mapToDTO(wdApplicationList);
-  }
+        });
+        return mapper.mapToDTO(wdApplicationList);
+    }
 
 }
