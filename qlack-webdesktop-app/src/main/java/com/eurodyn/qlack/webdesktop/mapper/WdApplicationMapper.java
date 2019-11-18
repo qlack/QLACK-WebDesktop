8864a7d58ee7c693bae911fea363dbb4a5acb364
Michail Bellas <michail.bellas@eurodyn.com>
2019-11-18 12:55:43 +0200

    Ignore id, checksum mapping

--- qlack-webdesktop-app/src/main/java/com/eurodyn/qlack/webdesktop/mapper/WdApplicationMapper.java
+++ qlack-webdesktop-app/src/main/java/com/eurodyn/qlack/webdesktop/mapper/WdApplicationMapper.java
@@ -3,13 +3,19 @@ package com.eurodyn.qlack.webdesktop.mapper;
 import com.eurodyn.qlack.webdesktop.dto.WdApplicationDTO;
 import com.eurodyn.qlack.webdesktop.model.WdApplication;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
 /**
  * Mapping interface for <tt>WdApplication</tt> entities and DTOs
  *
  * @author European Dynamics SA.
  */
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",  unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface WdApplicationMapper extends WdMapper<WdApplication, WdApplicationDTO> {
 
+  @Override
+  @Mapping(source = "id", target = "id", ignore = true)
+  @Mapping(source = "checksum", target = "checksum", ignore = true)
+  WdApplicationDTO mapToDTO(WdApplication entity);
 }
