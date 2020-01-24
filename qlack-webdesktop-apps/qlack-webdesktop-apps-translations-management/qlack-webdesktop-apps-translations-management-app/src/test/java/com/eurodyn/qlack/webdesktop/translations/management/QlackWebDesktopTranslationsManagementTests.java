71bb8d66e90b14d894c506527f6365860c0e8ec3
Roumpen Gkoltouchtsian <roumpen.gkoltouchtsian@eurodyn.com>
2020-01-24 18:10:31 +0200

    Refactored UI modules and fixed broken tests

--- qlack-webdesktop-apps/qlack-webdesktop-apps-translations-management/qlack-webdesktop-apps-translations-management-app/src/test/java/com/eurodyn/qlack/webdesktop/translations/management/QlackWebDesktopTranslationsManagementTests.java
+++ qlack-webdesktop-apps/qlack-webdesktop-apps-translations-management/qlack-webdesktop-apps-translations-management-app/src/test/java/com/eurodyn/qlack/webdesktop/translations/management/QlackWebDesktopTranslationsManagementTests.java
@@ -1,17 +1,9 @@
 package com.eurodyn.qlack.webdesktop.translations.management;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class QlackWebDesktopTranslationsManagementTests {
 
-
-  @Test
-  void contextLoads() {
-    assertTrue(true);
-  }
 
 }
