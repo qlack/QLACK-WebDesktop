b271b1d8e991c78f56cd33736cde4e3e30c78493
sotikele <xsotos@hotmail.com>
2020-02-26 12:45:20 +0200

    fixed the async call for translations

--- qlack-webdesktop-ui/angular/src/app/app.component.ts
+++ qlack-webdesktop-ui/angular/src/app/app.component.ts
@@ -1,8 +1,6 @@
-import {Component, Renderer2} from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
+import {Component} from '@angular/core';
 import {QngPubsubService} from '@qlack/qng-pubsub';
 import {WebdesktopService} from './webdesktop.service';
-import {UserDetailsDto} from './dto/user-details-dto';
 
 @Component({
   selector: 'app-root',
@@ -11,30 +9,14 @@ import {UserDetailsDto} from './dto/user-details-dto';
 })
 export class AppComponent {
   title = 'webDesktop';
-  userDetailsDto: any = new UserDetailsDto();
+  backgroundImage: any[];
 
-  constructor(private translate: TranslateService, private qPubSubService: QngPubsubService, private webDesktopService: WebdesktopService) {
-    this.webDesktopService.getUserAttributes().subscribe(userAttributeList => {
-      if (userAttributeList.firstName != null) {
-        this.userDetailsDto.firstName = userAttributeList.firstName.data;
-      }
-      if (userAttributeList.lastName != null) {
-        this.userDetailsDto.lastName = userAttributeList.lastName.data;
-      }
-      if (userAttributeList.defaultLanguage != null) {
-        this.userDetailsDto.defaultLanguage = userAttributeList.defaultLanguage.data;
-      } else {
-        this.userDetailsDto.defaultLanguage = 'en';
-      }
-      if (userAttributeList.profileImage != null) {
-        this.userDetailsDto.profileImage = userAttributeList.profileImage.bindata;
-      }
-      if (userAttributeList.backgroundImage != null) {
-        this.userDetailsDto.backgroundImage = userAttributeList.backgroundImage.bindata;
+  constructor(private qPubSubService: QngPubsubService, private webDesktopService: WebdesktopService) {
+    this.webDesktopService.getUserAttributeByName("backgroundImage").subscribe(attribute => {
+      if (attribute != null) {
+        this.backgroundImage = attribute.bindata;
       }
     });
-    //TODO call user profile and get his/hers preferred language
-    translate.setDefaultLang(this.userDetailsDto.defaultLanguage);
 
   }
 
--- qlack-webdesktop-ui/angular/src/app/app.module.ts
+++ qlack-webdesktop-ui/angular/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {
   MatButtonModule,
   MatDividerModule,
@@ -21,7 +21,7 @@ import {HeaderComponent} from './common/header/header.component';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {WebdesktopService} from "./webdesktop.service";
 import {StartMenuComponent} from "./common/start-menu/start-menu.component";
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {ResizableModule} from 'angular-resizable-element';
 import {QngPubsubModule} from '@qlack/qng-pubsub';
@@ -30,7 +30,15 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, "/apps/translations?lang=", "");
 }
-
+export function webDesktopServiceFactory(webDesktopService: WebdesktopService,translate: TranslateService): Function {
+  return () => webDesktopService.getUserAttributeByName("defaultLanguage").toPromise().then(attribute =>{
+    if (attribute != null){
+      translate.setDefaultLang(attribute.data);
+    }else{
+      translate.setDefaultLang("en");
+    }
+  }).catch((err: any) =>  Promise.resolve().then(() => translate.setDefaultLang("en")));
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -65,7 +73,15 @@ export function HttpLoaderFactory(http: HttpClient) {
     }),
     ResizableModule
   ],
-  providers: [WebdesktopService],
+  providers: [
+    WebdesktopService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: webDesktopServiceFactory,
+      deps: [WebdesktopService,TranslateService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent],
   entryComponents: [WidgetComponent]
 
