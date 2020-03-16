63be197e0c2d7d07206a679a2a087d1376304274
Margarita Tryfou <mtrifou@gmail.com>
2020-03-16 14:13:40 +0200

    add ProfileManager service

---
+++ qlack-webdesktop-common/src/main/java/com/eurodyn/qlack/webdesktop/common/service/ProfileManagerService.java
@@ -0,0 +1,23 @@
+package com.eurodyn.qlack.webdesktop.common.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * Access to the active profile.
+ */
+@Service
+public class ProfileManagerService {
+
+  @Value("${spring.profiles.active:}")
+  private String activeProfile;
+
+  /**
+   * Retrieves the name of the profile that is currently active.
+   * @return a string containing the active-s profiles comma separated.
+   * If no profile is active, then an empty string is return.
+   */
+  public String getActiveProfile() {
+    return activeProfile;
+  }
+}
